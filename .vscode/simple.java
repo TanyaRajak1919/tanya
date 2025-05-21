843f4e411bed93d628d042dcb45f28e6aa2efe91
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-05-21 09:49:49 +0530

    variables

---
+++ .vscode/simple.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class simple {
+    public static void main(String[] args) {
+        int age;
+        System.out.print("enter your age..!");
+        Scanner r=new Scanner(System.in);
+        age=r.nextInt();
+        if(age>=18)
+    }
+
+    {
+        System.out.print("Eligible for votes..!");
+    }System.out.print("Thank You..!");
+
+}
---
+++ Myapp/Main.java
@@ -0,0 +1,18 @@
+package Myapp;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "hii tanya";
+        System.out.println(name);
+
+    }
+
+    String name = "hii tanya";
+
+    void sayhello()
+
+    {
+        System.out.println(name);
+    }
+
+}
