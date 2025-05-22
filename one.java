290204c7bf56dc93acb01db375c2ac67221e526e
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-05-22 12:56:06 +0530

    hii

---
+++ one.java
@@ -0,0 +1,14 @@
+public class one {
+    public static void main(String[] args) {
+        int[] arr = { 2, 19, 10, 6 };
+        try {
+            System.out.println(arr[6]);
+
+        } catch (Exception e) {
+            System.out.println(e);
+
+        }
+        System.out.println("hiiii tanya");
+    }
+
+}
