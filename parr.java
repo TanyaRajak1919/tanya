47fc52144a37846800dddaef15ca23d5487bc74b
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-05-27 18:17:41 +0530

    print array

---
+++ parr.java
@@ -0,0 +1,14 @@
+public class parr {
+    public static void main(String[] args) {
+        int a[] = new int[3];
+        a[0] = 10;
+        a[1] = 19;
+        a[2] = 23;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(a[i] + " ");
+        }
+
+    }
+
+}
