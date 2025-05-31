a5885836e89b1ecb519cdf71a6aa2678660da150
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-05-31 09:13:32 +0530

    string

---
+++ Prepost.java
@@ -0,0 +1,16 @@
+public class Prepost {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.println("PrePost increment");
+        System.out.println(a++);
+        System.out.println(++a);
+
+
+    }
+    
+}
+{
+    System.out.println("PrePost Decremant..")
+    System.out.println(a++);System.out.println(++a);
+}
