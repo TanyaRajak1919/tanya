61d61dab7b73f44108694840f93781872d1bc23c
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-06-10 20:34:53 +0530

    conversion

---
+++ .vscode/convert.java
@@ -0,0 +1,11 @@
+public class convert {
+    public static void main(String[] args) {
+        int num1 = 476;
+        int num2 = 78656;
+
+        String str1 = Integer.toString(num1);
+        String str2 = Integer.toString(num2);
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
