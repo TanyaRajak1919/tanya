884cb20b0dbe4111f1a7cae768bedfeadfc7757f
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-06-14 16:32:10 +0530

    string convert

---
+++ Stringm.java
@@ -0,0 +1,9 @@
+//WAP to covert a string into lowercase 
+public class Stringm {
+    public static void main(String[] args) {
+        String name = "TANYA RAJAK";
+        name = name.toLowerCase();
+        System.out.println(name);
+    }
+    
+}
