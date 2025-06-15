e525ad488d2a9ff6afbe128adce9698364e3d916
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-06-15 18:20:37 +0530

    spaces

---
+++ Stringdt.java
@@ -0,0 +1,9 @@
+public class Stringdt {
+    public static void main(String[] args) {
+        
+ String myString = "This string contains double and triple spaces";
+ System.out.println(myString.indexOf("  "));
+ System.out.println(myString.indexOf(""));      
+    }
+    
+}
