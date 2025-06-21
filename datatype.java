429fdb11a429ebd252085a987dc3ff41bad785dc
TanyaRajak1919 <tanyarajak4646@gmail.com>
2025-06-21 18:31:17 +0530

    datatype

---
+++ datatype.java
@@ -0,0 +1,14 @@
+public class datatype {
+    public static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        short age3 = 20;
+        long ageDino = 566666L;
+        System.out.println(age);
+        System.out.println(ch);
+
+    }
+    
+}
